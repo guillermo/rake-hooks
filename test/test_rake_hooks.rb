aaada4bd89b9eec543fe9945078755c146364e56
Guillermo Álvarez <guillermo@cientifico.net>
2011-09-09 22:23:10 +0200

    Remove trailing spaces

--- test/test_rake_hooks.rb
+++ test/test_rake_hooks.rb
@@ -7,11 +7,11 @@ class Store
     @@data ||= ""
     @@data << string
   end
-  
+
   def self.to_s
     @@data || ""
   end
-  
+
   def self.clean
     @@data = ""
   end
@@ -28,45 +28,45 @@ class TestRakeHooks < Test::Unit::TestCase
     Rake::TaskManager.record_task_metadata = true
     Store.clean
   end
-  
+
   def test_after
     task  :task do  Store << "wadus"; end
     after :task do  Store << "way"  ;  end
-    
+
     execute(:task)
 
     assert_equal "wadusway", Store.to_s
   end
-  
+
   def test_after_multiple
     task  :task_one do  Store << "wadus"; end
     task  :task_two do  Store << "badus"; end
     after :task_one, :task_two do  Store << "way"  ;  end
-    
+
     execute(:task_one)
     assert_equal "wadusway", Store.to_s
-    
+
     Store.clean
-    
+
     execute(:task_two)
     assert_equal "badusway", Store.to_s
   end
-  
+
   def test_before
     task   :supertask do Store << "wadus" ; end
     before :supertask do Store << "super" ; end
-    
+
     execute(:supertask)
     assert_equal "superwadus", Store.to_s
   end
-  
+
   def test_after_and_before
     task   :super_task do Store << "wadus" ; end
     before :super_task do Store << "love " ; end
     before :super_task do Store << "I "    ; end
     after  :super_task do Store << " way"  ; end
     after  :super_task do Store << "."     ; end
-    
+
     execute(:super_task)
     assert_equal "I love wadus way.", Store.to_s
   end
